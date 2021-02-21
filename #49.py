e6d8d1ae9805390f38536421e3f1b75c08925203
Adittel <59428512+Adittel@users.noreply.github.com>
2021-02-20 18:14:37 -0800

    Update #49.py

--- #49.py
+++ #49.py
@@ -2,21 +2,22 @@
 #49. Group Anagrams
 
 # https://leetcode.com/problems/group-anagrams/
+
+#group anagrams
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        x=[str(sorted(a)) for a in strs]
         
         holder = dict()
 
-        for i in range(len(x)):
-            if x[i] in holder:
-                holder[x[i]].append(strs[i])
+        for i in range(len(strs)):
+            if str(sorted(strs[i])) in holder:
+                holder[str(sorted(strs[i]))].append(strs[i])
             else:
-                holder[x[i]] = [strs[i]]
+                holder[str(sorted(strs[i]))] = [strs[i]]
         main=[]
         print(holder)
         for key in (holder):
